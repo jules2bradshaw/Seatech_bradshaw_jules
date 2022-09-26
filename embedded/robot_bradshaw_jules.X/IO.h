0f777a6f46ea8aaff7e878d19da577489a787254
IUT GEII Toulon Table 6 <rct@univ-tln.fr>
2022-09-26 17:29:45 +0200

    modif fin de séance

---
+++ embedded/robot_bradshaw_jules.X/IO.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   IO.h
+ * Author: TABLE 6
+ *
+ * Created on 26 septembre 2022, 15:19
+ */
+
+#ifndef IO_H
+#define IO_H
+
+//Affectation des pins des LEDS
+#define LED_ORANGE _LATC10 
+#define LED_BLEUE _LATG7
+#define LED_BLANCHE _LATG6
+
+// Prototypes fonctions
+void InitIO();
+
+#endif /* IO_H */
