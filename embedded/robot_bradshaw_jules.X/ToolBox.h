be2a252baa9cfee14bfcca041b50f5e178f29943
IUT GEII Toulon Table 6 <rct@univ-tln.fr>
2022-09-28 09:23:03 +0200

    28/09/22.2

--- embedded/robot_bradshaw_jules.X/ToolBox.h
+++ embedded/robot_bradshaw_jules.X/ToolBox.h
@@ -1,23 +1,13 @@
-/* 
- * File:   ToolBox.h
- * Author: TABLE 6
- *
- * Created on 28 septembre 2022, 09:13
- */
+
 
 #ifndef TOOLBOX_H
 #define	TOOLBOX_H
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-
-
-
-#ifdef	__cplusplus
-}
-#endif
-
+#define PI 3.141592653589793
+float Abs(float value);
+float Max(float value, float value2);
+float Min(float value, float value2);
+float LimitToInterval(float value, float lowLimit, float highLimit);
+float RadianToDegree(float value);
+float DegreeToRadian(float value);
 #endif	/* TOOLBOX_H */
 
